94ded94cd90c01036cbcc63775e72b91140c6c5b
kareem <kareem.elshahawe@gmail.com>
2017-07-23 17:26:18 +0800

    Test autobuild

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Futbol"
+optionB = "Socor"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
